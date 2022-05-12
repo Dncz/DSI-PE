2c1b4e75bbe19a427790bde4a5f3cd3fced263c3
Dana Choque <alu0101328348@ull.edu.es>
2022-05-12 10:12:35 +0000

    comentarios

--- src/ejercicio12/server.ts
+++ src/ejercicio12/server.ts
@@ -7,20 +7,38 @@ import {execmd} from './comand';
  * http://localhost:3000/execmd?cmd=cat&args=-n ./src/ejercicio12/prueba.txt
  */
 
+/**
+ * Creamos un servidor con la invocación de la función express
+ */
 const app = express();
 
+/**
+ * Indicamos el directorio público de nuestro servidor
+ */
 app.use(express.static(join(__dirname, '../public')));
 
+/**
+ * Establecemos la ruta o punto de acceso
+ */
 app.get('/execmd', (req, res) => {
+  // si no se introduce el comando y los argumentos
   if (!req.query.cmd || !req.query.args) {
     res.status(406).send({err: 'A command has to be provided'});
   } else {
+    // ejecución de la función execmd
     execmd(req.query.cmd as string, req.query.args as string).then((result) => {
       res.send({result: result});
     }).catch((err) => {
       res.send({err: err});
     });
   }
+});
+
+/**
+ * En caso de ruta por defecto
+ */
+app.get('*', (_, res) => {
+  res.status(404).send('Not found');
 });
 
 app.listen(3000, () => {
